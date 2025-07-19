5da1f45324ccd670010b1a9a2991bdd865068079
Manya <manya111203@gmail.com>
2025-07-19 12:49:53 +0530

    Blog using Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // site: "https://example.com"
+  // Replace with your actual site URL
+  site: 'https://your-site.com',
+
+  integrations: [preact()]
+});
